35238a6927394141ca3de4b68b13aa3a1010f356
jessicaann <jessicaecottrell@gmail.com>
2017-08-04 11:18:29 -0700

    front end dev

--- public/client.js
+++ public/client.js
@@ -1,1 +1,14 @@
 var BASE_URL = "http://localhost:8080"
+
+//Page Reveals - Handles Clicks
+$(function () {
+    $(".jsPaxBttn").click(function() {
+     $(".userSignIn").removeClass("hidden");
+        $(".site-wrapper").addClass("hidden");
+    });
+    $(".jsAdminBttn").click(function() {
+        $(".AdminSignIn").removeClass("hidden");
+        $(".site-wrapper").addClass("hidden");
+    });
+    $(".")
+});
