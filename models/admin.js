18684b723b238d941939155e097bb08e6f7adf38
jessicaann <jessicaecottrell@gmail.com>
2018-01-07 09:51:39 -0700

    base_url updated

--- models/admin.js
+++ models/admin.js
@@ -1,27 +1,27 @@
 const mongoose = require('mongoose');
 
 
-//Admin Schema
+// Admin Schema
 const adminSchema = mongoose.Schema({
-    firstName: {type: String, required:  true},
-    lastName: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true}
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
 });
-//Admin API Reveal
-adminSchema.methods.apiRepr = function() {
-    return {
-        id: this.id,
-        firstName: this.firstName,
-        lastName: this.lastName,
-        fullName: this.fullName,
-        email: this.email
-    };
-}
+// Admin API Reveal
+adminSchema.methods.apiRepr = function () {
+  return {
+    id: this.id,
+    firstName: this.firstName,
+    lastName: this.lastName,
+    fullName: this.fullName,
+    email: this.email,
+  };
+};
 
-adminSchema.virtual('fullName').get(function() {
-    return `${this.firstName} ${this.lastName}`.trim()
+adminSchema.virtual('fullName').get(function () {
+  return `${this.firstName} ${this.lastName}`.trim();
 });
 
 const AdminModel = mongoose.model('Admin', adminSchema);
-module.exports= {AdminModel};
+module.exports = { AdminModel };
--- public/js/config.js
+++ public/js/config.js
@@ -1,2 +1,3 @@
-var BASE_URL = "http://localhost:8080";
-//"https://lit-savannah-21161.herokuapp.com" 
+var BASE_URL = "https://lit-savannah-21161.herokuapp.com";
+
+//"http://localhost:8080";
