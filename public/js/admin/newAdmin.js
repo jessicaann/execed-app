22e86a0893c4b09ff7431007fd986d933d95a4c7
jessicaann <jessicaecottrell@gmail.com>
2018-01-07 16:37:51 -0700

    polishing details

--- public/js/admin/newAdmin.js
+++ public/js/admin/newAdmin.js
@@ -1,37 +1,39 @@
-$(".newAdminForm").submit(function(event) {
-    event.preventDefault();
-    //get the info from the input
-    const firstName = $(".newAdminForm #firstName").val();
-    const lastName = $(".newAdminForm #lastName").val();
-    const email = $(".newAdminForm #email").val();
-    const password = $(".newAdminForm #password").val();
-    var getLoginSettings = {
-      url: BASE_URL + "/admins",
-      data: JSON.stringify({
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password
-      }),
-      dataType: "json",
-        headers: {
-            "content-type": "application/json"
-        },
-      method: "POST",
-      success: function(res){var transElement = 
-            `<div class="positive-msg-display">Admin created</div>`;
-            $(".msg-display").html(transElement);}
-    }
-    $.ajax(getLoginSettings);
-})
+$('.newAdminForm').submit((event) => {
+  event.preventDefault();
+  // get the info from the input
+  const firstName = $('.newAdminForm #firstName').val();
+  const lastName = $('.newAdminForm #lastName').val();
+  const email = $('.newAdminForm #email').val();
+  const password = $('.newAdminForm #password').val();
+  const getLoginSettings = {
+    url: `${BASE_URL}/admins`,
+    data: JSON.stringify({
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+    dataType: 'json',
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'POST',
+    success(res) {
+      const transElement =
+            '<div class="positive-msg-display">Admin created</div>';
+      $('.msg-display').html(transElement);
+    },
+  };
+  $.ajax(getLoginSettings);
+});
 
-function displayName(){
-    $('.username span').text(localStorage.getItem('adminName'));
+function displayName() {
+  $('.username span').text(localStorage.getItem('adminName'));
 }
 
-//Watch Page Load
+// Watch Page Load
 function watchPageLoad() {
-    displayName();
+  displayName();
 }
 
 $(watchPageLoad);
--- public/js/admin/newUser.js
+++ public/js/admin/newUser.js
@@ -1,71 +1,71 @@
-$(".newUserForm").submit(function(event) {
-    event.preventDefault();
-    //get the info from the input
-    const firstName = $(".newUserForm #firstName").val();
-    const lastName = $(".newUserForm #lastName").val();
-    const email = $(".newUserForm #email").val();
-    const password = $(".newUserForm #password").val();
-    const schedules = $(".newUserForm #schedules").val();
-    var getUserSettings = {
-      url: BASE_URL + "/users",
-      data: JSON.stringify({
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password,
-        schedules: schedules
-      }),
-      dataType: "json",
-        headers: {
-            "content-type": "application/json"
-        },
-      method: "POST",
-      error: function(res){var transElement = 
+$('.newUserForm').submit((event) => {
+  event.preventDefault();
+  // get the info from the input
+  const firstName = $('.newUserForm #firstName').val();
+  const lastName = $('.newUserForm #lastName').val();
+  const email = $('.newUserForm #email').val();
+  const password = $('.newUserForm #password').val();
+  const schedules = $('.newUserForm #schedules').val();
+  let getUserSettings = {
+    url: `${BASE_URL  }/users`,
+    data: JSON.stringify({
+      firstName,
+      lastName,
+      email,
+      password,
+      schedules,
+    }),
+    dataType: 'json',
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'POST',
+    error(res){var transElement = 
             `<div class="negative-msg-display">Cannot create user</div>`;
-            $(".msg-display").html(transElement);},
-      success: function(res){var transElement = 
+    $(".msg-display").html(transElement);},
+    success(res){var transElement = 
             `<div class="positive-msg-display">User created</div>`;
-            $(".msg-display").html(transElement);}
-    }
-    $.ajax(getUserSettings);
-})
-//get Schedules for the dropdown
+    $(".msg-display").html(transElement);},
+  };
+  $.ajax(getUserSettings);
+});
+// get Schedules for the dropdown
 function getSchedules(successCallback) {
-    var getScheduleSettings = {
-      url: BASE_URL + "/schedules/admin/" + localStorage.getItem("adminId"),
-      data: JSON.stringify({}),
-      dataType: "json",
-        headers: {
-            "content-type": "application/json"
-        },
-      method: "GET",
-      success: function(res){
-          console.log(res);
-          successCallback(res);
-      }
-    }
-    $.ajax(getScheduleSettings);
-  }
+  const getScheduleSettings = {
+    url: `${BASE_URL }/schedules/admin/${localStorage.getItem('adminId')}`,
+    data: JSON.stringify({}),
+    dataType: 'json',
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'GET',
+    success(res) {
+      console.log(res);
+      successCallback(res);
+    },
+  };
+  $.ajax(getScheduleSettings);
+}
 // Display sessions in dropdown
-  function displaySchedules (response) {
-     console.log(response);
-    console.log("displaySchedules");
-      var transElement = '';
-    if(response.schedules) {
-        response.schedules.forEach(function(schedule) {
-            transElement += `<option value="${schedule.id}">${schedule.title}</option>`;
-        })
-    }
-    $('#schedules').html(transElement);
+function displaySchedules(response) {
+  console.log(response);
+  console.log('displaySchedules');
+  let transElement = '';
+  if (response.schedules) {
+    response.schedules.forEach((schedule) => {
+      transElement += `<option value="${schedule.id}">${schedule.title}</option>`;
+    });
   }
-function displayName(){
-    $('.username span').text(localStorage.getItem('adminName'));
+  $('#schedules').html(transElement);
+}
+function displayName() {
+  $('.username span').text(localStorage.getItem('adminName'));
 }
 
-//Watch Page Load
+// Watch Page Load
 function watchPageLoad() {
-    displayName();
-    getSchedules(displaySchedules);
+  displayName();
+  getSchedules(displaySchedules);
 }
 
 $(watchPageLoad);
