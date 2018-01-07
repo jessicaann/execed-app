84c672a4530a030722b3cc1840ce91801e7b4c7a
jessicaann <jessicaecottrell@gmail.com>
2018-01-07 16:14:57 -0700

    fix schedule display

--- public/js/admin/admins.js
+++ public/js/admin/admins.js
@@ -1,43 +1,43 @@
-//Get Admins
+// Get Admins
 function getAdmins(successCallback) {
-    var getAdminSettings = {
-      url: BASE_URL + "/admins",
-      data: JSON.stringify({}),
-      dataType: "json",
-        headers: {
-            "content-type": "application/json"
-        },
-      method: "GET",
-      success: function(res){
-          successCallback(res);
-      }
-    }
-    $.ajax(getAdminSettings);
-  }
+  const getAdminSettings = {
+    url: `${BASE_URL }/admins`,
+    data: JSON.stringify({}),
+    dataType: 'json',
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'GET',
+    success(res) {
+      successCallback(res);
+    },
+  };
+  $.ajax(getAdminSettings);
+}
 // Display admins
-  function displayAdmins (response) {
-      var transElement = '';
-    if(response.admins) {
-        response.admins.forEach(function(admin) {
-            transElement += `<a href="./admin_edit_remove.html?adminId=${admin.id}" class="col-xs-6 col-sm-3 placeholder js-edit">
+function displayAdmins(response) {
+  let transElement = '';
+  if (response.admins) {
+    response.admins.forEach((admin) => {
+      transElement += `<a href="./admin_edit_remove.html?adminId=${admin.id}" class="col-xs-6 col-sm-3 placeholder js-edit">
             <img class="img-responsive" src="../img/defaultavatar.png" width="200" height="200">
             <h4>${admin.fullName}</h4>
             <span class="text-muted">${admin.email}</span>
             </a>`;
-        })
-    }
-    $(".adminsDisplay").html(transElement);
+    });
   }
-  
-
-function displayName(){
-    $('.username span').text(localStorage.getItem('adminName'));
+  $('.adminsDisplay').html(transElement);
 }
 
-//Watch Page Load
+
+function displayName() {
+  $('.username span').text(localStorage.getItem('adminName'));
+}
+
+// Watch Page Load
 function watchAdminsPageLoad() {
-    displayName();
-    getAdmins(displayAdmins);
+  displayName();
+  getAdmins(displayAdmins);
 }
 
 $(watchAdminsPageLoad);
--- public/js/admin/dashboard.js
+++ public/js/admin/dashboard.js
@@ -1,50 +1,50 @@
-//Get Schedules for Admin
+// Get Schedules for Admin
 function getSchedules(successCallback) {
-    var getScheduleSettings = {
-      url: BASE_URL + "/schedules/admin/" + localStorage.getItem("adminId"),
-      data: JSON.stringify({}),
-      dataType: "json",
-        headers: {
-            "content-type": "application/json"
-        },
-      method: "GET",
-      success: function(res){
-          console.log(res);
-          successCallback(res);
-      }
-    }
-    $.ajax(getScheduleSettings);
-  }
+  const getScheduleSettings = {
+    url: `${BASE_URL }/schedules/admin/${localStorage.getItem('adminId')}`,
+    data: JSON.stringify({}),
+    dataType: 'json',
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'GET',
+    success(res) {
+      console.log(res);
+      successCallback(res);
+    },
+  };
+  $.ajax(getScheduleSettings);
+}
 // Display Schedules
-  function displaySchedules (response) {
-     console.log(response);
-    console.log("displaySchedules");
-      var transElement = '';
-    if(response.schedules) {
-        response.schedules.forEach(function(schedule) {
-            transElement += `<a href="../schedule/scheduleDisplay.html?scheduleId=${schedule.id}" class="col-xs-6 col-sm-3 placeholder js-edit">
+function displaySchedules(response) {
+  console.log(response);
+  console.log('displaySchedules');
+  let transElement = '';
+  if (response.schedules) {
+    response.schedules.forEach((schedule) => {
+      transElement += `<a href="../schedule/scheduleDisplay.html?scheduleId=${schedule.id}" class="col-xs-6 col-sm-3 placeholder js-edit">
                 <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
                 <h4>${schedule.title}</h4>
                 <span class="text-muted">${schedule.dates}</span>
             </a>`;
-        })
-    }
-    $(".schedulesDisplay").html(transElement);
+    });
   }
-  
-  $(".nav a").on("click", function() {
-    $(".nav").find(".active").removeClass("active");
-    $(this).parent().addClass("active");
-  });
-
-function displayName(){
-    $('.username span').text(localStorage.getItem('adminName'));
+  $('.schedulesDisplay').html(transElement);
 }
 
-//Watch Dashboard Page Load
+$('.nav a').on('click', function () {
+  $('.nav').find('.active').removeClass('active');
+  $(this).parent().addClass('active');
+});
+
+function displayName() {
+  $('.username span').text(localStorage.getItem('adminName'));
+}
+
+// Watch Dashboard Page Load
 function watchDashboardPageLoad() {
-    displayName();
-    getSchedules(displaySchedules);
+  displayName();
+  getSchedules(displaySchedules);
 }
 
 $(watchDashboardPageLoad);
