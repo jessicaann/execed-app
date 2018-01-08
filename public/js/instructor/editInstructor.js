ede54539dfbbcacf265561f4977b78e8138849eb
jessicaann <jessicaecottrell@gmail.com>
2018-01-07 18:45:03 -0700

    instructor redirects

--- public/js/instructor/editInstructor.js
+++ public/js/instructor/editInstructor.js
@@ -1,100 +1,102 @@
-//do the call that gets the instructor info and populates it into the form based on the email on page load
+// do the call that gets the instructor info and populates it into the form based on the email on page load
 function getInstructors(successCallback) {
-    localStorage.setItem('editInstrId', location.search.split("?")[1].split("=")[1]);
-     var getInstructorSettings = {
-      url: BASE_URL + "/instructors/" + localStorage.getItem('editInstrId'),
-      data: JSON.stringify({}),
-      dataType: "json",
-        headers: {
-            "content-type": "application/json"
-        },
-      method: "GET",
-      success: function(res){
-          successCallback(res);
-      }
-    }
-    $.ajax(getInstructorSettings);
-  }
+  localStorage.setItem('editInstrId', location.search.split('?')[1].split('=')[1]);
+  const getInstructorSettings = {
+    url: `${BASE_URL}/instructors/${localStorage.getItem('editInstrId')}`,
+    data: JSON.stringify({}),
+    dataType: 'json',
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'GET',
+    success(res) {
+      successCallback(res);
+    },
+  };
+  $.ajax(getInstructorSettings);
+}
 
 $(getInstructors(displayInstructors));
 
-function displayInstructors(response){
-    const {email, firstName, lastName} = response;
-    $('#firstName').val(firstName);
-    $('#email').val(email);
-    $('#lastName').val(lastName);
+function displayInstructors(response) {
+  const { email, firstName, lastName } = response;
+  $('#firstName').val(firstName);
+  $('#email').val(email);
+  $('#lastName').val(lastName);
 }
 
-$(".editInstructorForm").submit(function(event) {
-    event.preventDefault();
-    //get the info from the input
-    const firstName = $(".editInstructorForm #firstName").val();
-    const lastName = $(".editInstructorForm #lastName").val();
-    const email = $(".editInstructorForm #email").val();
-    
-    var getInstructorSettings = {
-      url: BASE_URL + "/instructors/profile/" + localStorage.getItem('editInstrId'),
-      data: JSON.stringify({
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        id: localStorage.getItem('editInstrId')
-      }),
-      dataType: "json",
-        headers: {
-            "content-type": "application/json"
-        },
-      method: "PUT",
-      error: function(response){
-          console.log("Unable to save changes", response);
-          var transElement = 
-            `<div class="negative-msg-display">Unable to save changes</div>`;
-            $(".msg-display").html(transElement);
-      },
-      success: function(response){
-          console.log("Item removed", response);
-          var transElement = 
-            `<div class="positive-msg-display">Changes saved</div>`;
-            $(".msg-display").html(transElement);
-      }
-    }
-    $.ajax(getInstructorSettings);
-})
-//delete call
-$(".delete").click(function(event) {
-    event.preventDefault();
- 
-    var getInstructorSettings = {
-      url: BASE_URL + "/instructors/profile/" + localStorage.getItem('editInstrId'),
-      data: JSON.stringify({}),
-      dataType: "json",
-        headers: {
-            "content-type": "application/json"
-        },
-      method: "DELETE",
-      error: function(response){
-          console.log("Unable to delete item", response);
-          var transElement = 
-            `<div class="negative-msg-display"Unable to delete item</div>`;
-            $(".msg-display").html(transElement);
-      },
-      success: function(response){
-          console.log("Item removed", response);
-          var transElement = 
-            `<div class="positive-msg-display">Instructor removed</div>`;
-            $(".editInstructor").html(transElement);
-      }
-    }
-    $.ajax(getInstructorSettings);
-})
+$('.editInstructorForm').submit((event) => {
+  event.preventDefault();
+  // get the info from the input
+  const firstName = $('.editInstructorForm #firstName').val();
+  const lastName = $('.editInstructorForm #lastName').val();
+  const email = $('.editInstructorForm #email').val();
 
-function displayName(){
-    $('.username span').text(localStorage.getItem('adminName'));
+  const getInstructorSettings = {
+    url: `${BASE_URL}/instructors/${localStorage.getItem('editInstrId')}`,
+    data: JSON.stringify({
+      firstName,
+      lastName,
+      email,
+      id: localStorage.getItem('editInstrId'),
+    }),
+    dataType: 'json',
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'PUT',
+    error(response) {
+      console.log('Unable to save changes', response);
+      let transElement =
+            '<div class="negative-msg-display">Unable to save changes</div>';
+      $('.msg-display').html(transElement);
+    },
+    success(response) {
+      console.log('Item removed', response);
+      let transElement =
+            '<div class="positive-msg-display">Changes saved</div>';
+      $('.msg-display').html(transElement);
+      location.href='./instructors.html';
+    },
+  };
+  $.ajax(getInstructorSettings);
+});
+// delete call
+$('.delete').click((event) => {
+  event.preventDefault();
+
+  const getInstructorSettings = {
+    url: `${BASE_URL }/instructors/${ localStorage.getItem('editInstrId')}`,
+    data: JSON.stringify({}),
+    dataType: 'json',
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'DELETE',
+    error(response) {
+      console.log('Unable to delete item', response);
+      let transElement =
+            '<div class="negative-msg-display"Unable to delete item</div>';
+      $('.msg-display').html(transElement);
+    },
+    success(response) {
+      console.log('Item removed', response);
+      let transElement =
+            '<div class="positive-msg-display">Instructor removed</div>';
+      $('.editInstructor').html(transElement);
+      location.href='./instructors.html';
+    },
+  };
+  $.ajax(getInstructorSettings);
+});
+
+function displayName() {
+  $('.username span').text(localStorage.getItem('adminName'));
 }
 
-//Watch Page Load
+// Watch Page Load
 function watchPageLoad() {
-    displayName();
+  displayName();
 }
 
 $(watchPageLoad);
--- public/js/instructor/newInstructor.js
+++ public/js/instructor/newInstructor.js
@@ -1,56 +1,57 @@
-$(".newInstructorForm").submit(function(event) {
-    event.preventDefault();
-    //get the info from the input
-    const firstName = $(".newInstructorForm #firstName").val().trim();
-    const lastName = $(".newInstructorForm #lastName").val().trim();
-    const email = $(".newInstructorForm #email").val().trim();
-    var getInstructorSettings = {
-      url: BASE_URL + "/instructors",
-      data: JSON.stringify({
-        firstName: firstName,
-        lastName: lastName,
-        email: email
-      }),
-      dataType: "json",
-        headers: {
-            "content-type": "application/json"
-        },
-      method: "POST",
-      success: function(res){var transElement = 
+$('.newInstructorForm').submit((event) => {
+  event.preventDefault();
+  // get the info from the input
+  const firstName = $('.newInstructorForm #firstName').val().trim();
+  const lastName = $('.newInstructorForm #lastName').val().trim();
+  const email = $('.newInstructorForm #email').val().trim();
+  let getInstructorSettings = {
+    url: `${BASE_URL  }/instructors`,
+    data: JSON.stringify({
+      firstName,
+      lastName,
+      email,
+    }),
+    dataType: 'json',
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'POST',
+    success(res){var transElement = 
             `<div class="positive-msg-display">Instructor created</div>`;
-            $(".msg-display").html(transElement);}
-      }
-    $.ajax(getInstructorSettings);
-})
+            $(".msg-display").html(transElement);},
+  };
+  $.ajax(getInstructorSettings);
+  location.href = './instructors.html';
+});
 
-$(".logout").click(function(event) {
-    event.preventDefault();
-    //delete the admin's session
-    const adminId = localStorage.getItem('adminId');
-    var getLogoutSettings = {
-      url: BASE_URL + "/admins/session",
-      data: JSON.stringify({
-        email: email,
-        password: password
-      }),
-      dataType: "json",
-        headers: {
-            "content-type": "application/json"
-        },
-      method: "DELETE",
-      success: function(res){
+$('.logout').click((event) => {
+  event.preventDefault();
+  // delete the admin's session
+  const adminId = localStorage.getItem('adminId');
+  let getLogoutSettings = {
+    url: `${BASE_URL  }/admins/session`,
+    data: JSON.stringify({
+      email,
+      password,
+    }),
+    dataType: 'json',
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'DELETE',
+    success(res){
           location.href="../logout.html";
-      }
-    }
-    $.ajax(getLogoutSettings);
-})
-function displayName(){
-    $('.username span').text(localStorage.getItem('adminName'));
+      },
+  };
+  $.ajax(getLogoutSettings);
+});
+function displayName() {
+  $('.username span').text(localStorage.getItem('adminName'));
 }
 
-//Watch Page Load
+// Watch Page Load
 function watchPageLoad() {
-    displayName();
+  displayName();
 }
 
 $(watchPageLoad);
