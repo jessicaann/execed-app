0f3fec8a996f1f22d9ecd10593af020c05fb523d
jessicaann <jessicaecottrell@gmail.com>
2018-01-07 18:31:25 -0700

    users redirect

--- public/js/admin/editUser.js
+++ public/js/admin/editUser.js
@@ -1,149 +1,154 @@
-//do the call that gets the user info and populates it into the form based on the email on page load
+// do the call that gets the user info and populates it into the form based on the email on page load
 function getUsers(successCallback) {
-    localStorage.setItem('editUserId', location.search.split("?")[1].split("=")[1]);
-    var getUserSettings = {
-      url: BASE_URL + "/users/" + localStorage.getItem('editUserId'),
-      data: JSON.stringify({}),
-      dataType: "json",
-        headers: {
-            "content-type": "application/json"
-        },
-      method: "GET",
-      success: function(res){
-          successCallback(res);
-      }
-    }
-    $.ajax(getUserSettings);
-  }
+  localStorage.setItem('editUserId', location.search.split('?')[1].split('=')[1]);
+  const getUserSettings = {
+    url: `${BASE_URL}/users/${localStorage.getItem('editUserId')}`,
+    data: JSON.stringify({}),
+    dataType: 'json',
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'GET',
+    success(res) {
+      successCallback(res);
+    },
+  };
+  $.ajax(getUserSettings);
+}
 
 $(getUsers(displayUsers));
 
-function displayUsers(response){
-    const {email, firstName, lastName, schedules} = response;
-    var scheduleTitles = [];
-    schedules.forEach(function(schedule){
-        scheduleTitles.push(schedule.title);
-    });
-    $('#firstName').val(firstName);
-    $('#email').val(email);
-    $('#lastName').val(lastName);
+function displayUsers(response) {
+  const {
+    email, firstName, lastName, schedules,
+  } = response;
+  const scheduleTitles = [];
+  schedules.forEach((schedule) => {
+    scheduleTitles.push(schedule.title);
+  });
+  $('#firstName').val(firstName);
+  $('#email').val(email);
+  $('#lastName').val(lastName);
 
-    getSchedules(displaySchedules, schedules);
+  getSchedules(displaySchedules, schedules);
 }
 
-$(".editUserForm").submit(function(event) {
-    event.preventDefault();
-    //get the info from the input
-    const firstName = $(".editUserForm #firstName").val();
-    const lastName = $(".editUserForm #lastName").val();
-    const email = $(".editUserForm #email").val();
-    const password = $(".editUserForm #password").val();
-    const schedules = $(".editUserForm #schedules").val();
+$('.editUserForm').submit((event) => {
+  event.preventDefault();
+  // get the info from the input
+  const firstName = $('.editUserForm #firstName').val();
+  const lastName = $('.editUserForm #lastName').val();
+  const email = $('.editUserForm #email').val();
+  const password = $('.editUserForm #password').val();
+  const schedules = $('.editUserForm #schedules').val();
 
-    
-    var getUserSettings = {
-      url: BASE_URL + "/users/" + localStorage.getItem('editUserId'),
-      data: JSON.stringify({
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password,
-        schedules: schedules,
-        id: localStorage.getItem('editUserId')
-      }),
-      dataType: "json",
-        headers: {
-            "content-type": "application/json"
-        },
-      method: "PUT",
-      error: function(response){
-        var transElement = 
-        `<div class="negative-msg-display">Unable to update user</div>`;
-        $(".msg-display").html(transElement);},
-      success: function(response){
-        var transElement = 
-        `<div class="positive-msg-display">User updated</div>`;
-        $(".msg-display").html(transElement);}
-    }
-    $.ajax(getUserSettings);
-})
+
+  const getUserSettings = {
+    url: `${BASE_URL}/users/${localStorage.getItem('editUserId')}`,
+    data: JSON.stringify({
+      firstName,
+      lastName,
+      email,
+      password,
+      schedules,
+      id: localStorage.getItem('editUserId'),
+    }),
+    dataType: 'json',
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'PUT',
+    error(response) {
+      let transElement =
+        '<div class="negative-msg-display">Unable to update user</div>';
+      $('.msg-display').html(transElement); 
+},
+    success(response) {
+      let transElement =
+        '<div class="positive-msg-display">User updated</div>';
+      $('.msg-display').html(transElement);
+      location.href='./users.html'
+},
+  };
+  $.ajax(getUserSettings);
+});
 
 function getSchedules(successCallback, scheduleTitles) {
-    var getScheduleSettings = {
-      url: BASE_URL + "/schedules",
-      data: JSON.stringify({}),
-      dataType: "json",
-        headers: {
-            "content-type": "application/json"
-        },
-      method: "GET",
-      success: function(res){
-          console.log(res);
-          successCallback(res, scheduleTitles);
-      }
-    }
-    $.ajax(getScheduleSettings);
-  }
+  const getScheduleSettings = {
+    url: `${BASE_URL}/schedules`,
+    data: JSON.stringify({}),
+    dataType: 'json',
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'GET',
+    success(res) {
+      console.log(res);
+      successCallback(res, scheduleTitles);
+    },
+  };
+  $.ajax(getScheduleSettings);
+}
 // Display Schedules
-function displaySchedules (response, scheduleTitles) {
-    var transElement = '';
-    if(response.schedules) {
-        response.schedules.forEach(function(schedule) {
-            let isSelected = false;
-            scheduleTitles.forEach(function(_schedule){
-                if(schedule.id === _schedule.id){
-                   isSelected = true;
-                }
-            })
-            if(isSelected){
-                transElement += `<option selected value="${schedule.id}">${schedule.title}</option>`;
-            }else{
-                transElement += `<option value="${schedule.id}">${schedule.title}</option>`;
-            }
-        })
-    }
-    $('#schedules').html(transElement);
-}
-
-//delete call
-$(".delete").click(function(event) {
-    event.preventDefault();
-    var deleteConfirm = confirm('Delete this item?');
-    if (!deleteConfirm) {
-        return
-    }
-    var getUserSettings = {
-      url: BASE_URL + "/users/" + localStorage.getItem('editUserId'),
-      data: JSON.stringify({}),
-      dataType: "json",
-        headers: {
-            "content-type": "application/json"
-        },
-      method: "DELETE",
-      error: function(response){
-        console.log("Cannot delete item", response);
-        var transElement = 
-          `<div class="negative-msg-display">Cannot delete item</div>`;
-          $(".msg-display").html(transElement);
-      },
-      success: function(response){
-          console.log("Item removed", response);
-          var transElement = 
-            `<div class="itemdeleted">User deleted</div>`;
-            $(".msg-display").html(transElement);
+function displaySchedules(response, scheduleTitles) {
+  let transElement = '';
+  if (response.schedules) {
+    response.schedules.forEach((schedule) => {
+      let isSelected = false;
+      scheduleTitles.forEach((_schedule) => {
+        if (schedule.id === _schedule.id) {
+          isSelected = true;
+        }
+      });
+      if (isSelected) {
+        transElement += `<option selected value="${schedule.id}">${schedule.title}</option>`;
+      } else {
+        transElement += `<option value="${schedule.id}">${schedule.title}</option>`;
       }
-    }
-    $.ajax(getUserSettings);
-})
-
-function displayName(){
-    $('.username span').text(localStorage.getItem('adminName'));
+    });
+  }
+  $('#schedules').html(transElement);
 }
 
-//Watch Page Load
+// delete call
+$('.delete').click((event) => {
+  event.preventDefault();
+  const deleteConfirm = confirm('Delete this item?');
+  if (!deleteConfirm) {
+    return;
+  }
+  const getUserSettings = {
+    url: `${BASE_URL}/users/${localStorage.getItem('editUserId')}`,
+    data: JSON.stringify({}),
+    dataType: 'json',
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'DELETE',
+    error(response) {
+      console.log('Cannot delete item', response);
+      let transElement =
+          '<div class="negative-msg-display">Cannot delete item</div>';
+      $('.msg-display').html(transElement);
+    },
+    success(response) {
+      console.log('Item removed', response);
+      let transElement =
+            '<div class="itemdeleted">User deleted</div>';
+      $('.msg-display').html(transElement);
+    },
+  };
+  $.ajax(getUserSettings);
+});
+
+function displayName() {
+  $('.username span').text(localStorage.getItem('adminName'));
+}
+
+// Watch Page Load
 function watchPageLoad() {
-    displayName();
-    getUsers(displayUsers);
+  displayName();
+  getUsers(displayUsers);
 }
 
 $(watchPageLoad);
--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -1,132 +1,130 @@
 const express = require('express');
+
 const router = express.Router();
 
 const bodyParser = require('body-parser');
+
 const jsonParser = bodyParser.json();
 
-const {AdminModel} = require('../models/admin');
+const { AdminModel } = require('../models/admin');
 
-//Admin Login - Create a Session
+// Admin Login - Create a Session
 router.post('/session', jsonParser, (req, res) => {
-    console.log(req.body);
-    const requiredFields = ['email', 'password'];
-    for (let i=0; i<requiredFields.length; i++) {
-        const field = requiredFields[i];
-        if (!(field in req.body)) {
-            const message = `Missing \`${field}\` in request body`
-            console.error(message);
-            return res.status(400).send(message);
-        }
+  console.log(req.body);
+  const requiredFields = ['email', 'password'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
     }
-    AdminModel
-    .findOne({email: req.body.email})
+  }
+  AdminModel
+    .findOne({ email: req.body.email })
     .exec()
-    .then(admin => {
-        if(admin) {
-            //validate the password
-            if(admin.password !== req.body.password) {
-                res.status(404).json({message: 'Invalid login'})
-            }
-            else {
-                //create the session
-                res.status(200).json({accessToken: admin.id, username: admin.fullName})
-            }
+    .then((admin) => {
+      if (admin) {
+        // validate the password
+        if (admin.password !== req.body.password) {
+          res.status(404).json({ message: 'Invalid login' });
+        } else {
+          // create the session
+          res.status(200).json({ accessToken: admin.id, username: admin.fullName });
         }
-        else {
-            res.status(404).json({message: 'Invalid login'})
-        }
-    })
-})
-//Delete the Session
+      } else {
+        res.status(404).json({ message: 'Invalid login' });
+      }
+    });
+});
+// Delete the Session
 router.delete('/session', (req, res) => {
-    res.status(204).send
-})
+  res.status(204).send;
+});
 
-//Get all admin accounts
+// Get all admin accounts
 router.get('/', (req, res) => {
-    AdminModel
-        .find()
-        .exec()
-        .then(admins => {
-            res.json({
-                admins: admins.map(
-                (admin) => admin.apiRepr())
-            });
+  AdminModel
+    .find()
+    .exec()
+    .then((admins) => {
+      res.json({
+        admins: admins.map(admin => admin.apiRepr()),
+      });
     })
-    .catch(
-      err => {
+    .catch((err) => {
         console.error(err);
-        res.status(500).json({message: 'Internal server error'});
+        res.status(500).json({ message: 'Internal server error' });
       });
 });
 
-//Get individual admin accounts
+// Get individual admin accounts
 router.get('/:id', (req, res) => {
-    AdminModel
-        .findById(req.params.id)
-        .exec()
-        .then(admin => res.json(admin.apiRepr()))
-        .catch(err => {
-        console.error(err);
-            res.status(500).json({message: 'Internal server error'})
+  AdminModel
+    .findById(req.params.id)
+    .exec()
+    .then(admin => res.json(admin.apiRepr()))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
     });
 });
-//Create new admin accounts
+// Create new admin accounts
 router.post('/', jsonParser, (req, res) => {
-    const requiredFields = ['firstName', 'lastName', 'email', 'password'];
-    for (let i=0; i<requiredFields.length; i++) {
-        const field = requiredFields[i];
-        if (!(field in req.body)) {
-            const message = `Missing \`${field}\` in request body`
-            console.error(message);
-            return res.status(400).send(message);
-        }
+  const requiredFields = ['firstName', 'lastName', 'email', 'password'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
     }
-    AdminModel
+  }
+  AdminModel
     .create({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password})
-    .then(
-    admin => res.status(201).json(admin.apiRepr()))
-    .catch(err => {
-        console.error(err);
-        res.status(500).json({message: 'Internal server error'});
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password 
+})
+    .then(admin => res.status(201).json(admin.apiRepr()))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
     });
 });
-//Update admin accounts
+// Update admin accounts
 router.put('/profile/:id', jsonParser, (req, res) => {
-    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     const message = (
       `Request path id (${req.params.id}) and request body id ` +
       `(${req.body.id}) must match`);
     console.error(message);
-    res.status(400).json({message: message});
+    res.status(400).json({ message });
   }
-    const toUpdate = {};
-    const updateablefields = ['firstName', 'lastName', 'email', 'password'];
-    
-    updateablefields.forEach(field => {
-        if (field in req.body && req.body[field] !== "") {
-            toUpdate[field] = req.body[field];
-        }
-    });
-    AdminModel
-        .findByIdAndUpdate(req.params.id, {$set: toUpdate})
-        .exec()
-        .then(admin => res.status(200).json(toUpdate))
-        .catch(err => res.status(500).json({message: 'Internal server error'}));
+  const toUpdate = {};
+  const updateablefields = ['firstName', 'lastName', 'email', 'password'];
+
+  updateablefields.forEach((field) => {
+    if (field in req.body && req.body[field] !== '') {
+      toUpdate[field] = req.body[field];
+    }
+  });
+  AdminModel
+    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+    .exec()
+    .then(admin => res.status(200).json(toUpdate))
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
 
-//Delete Admin Accounts
+// Delete Admin Accounts
 router.delete('/:id', (req, res) => {
-    AdminModel
-        .findByIdAndRemove(req.params.id)
-        .exec()
-        .then(admin => res.status(204).end())
-        .catch(err => res.status(500).json({message: 'Internal server error'}));
+  AdminModel
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(admin => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
 
-//export the router
+// export the router
 module.exports = router;
